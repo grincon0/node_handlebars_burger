ad77d875172795a1e351ef819583eebfac67e450
Wake <georgerincon2@gmail.com>
2018-12-17 06:47:25 -0500

    created early version of server

---
+++ server.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+const exporess = require('express');
+const exphbr = require('express-handlebars');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', exphbr({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+
+app.listen(process.env.PORT || 3360, ()=> {
+    console.log('Server running on port: 3360')
+});
