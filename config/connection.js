cf11a6dc1e59b208f7abfb4e90ccd05a99eeb8aa
Wake <georgerincon2@gmail.com>
2019-01-05 03:23:33 -0500

    bug fix test

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: 'De*l%ta$Ra!y#Edge7!8$9',
+    password: '',
     database: 'burgers_db'
 });
 
